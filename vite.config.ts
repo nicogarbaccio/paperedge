a10015c108ee7b0eb1e9db92370e09ef08a3707c
Nico Garbaccio <garbaccio20@gmail.com>
2025-08-03 21:37:26 -0400

    fix: add sharp module and update build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}) 
+  optimizeDeps: {
+    include: ['sharp'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/sharp/, /node_modules/],
+    },
+  },
+})
